d57f412278bb04b1b10bcd915488ef4663300571
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2022-06-09 22:49:29 +0200

    Simulation: align files on several oligos

--- compression/dna/codec/fasta/align_files_on_header.py
+++ compression/dna/codec/fasta/align_files_on_header.py
@@ -3,33 +3,38 @@
 """
 @author : Romain Graux
 @date : 2022 June 09, 17:28:55
-@last modified : 2022 June 09, 22:07:45
+@last modified : 2022 June 09, 22:48:38
 """
 
+import re
 from os import system
 from glob import glob
 
-def extract_header(path):
+def extract_n_first(path, n):
     """
-    Extract headers from a file
+    Extract infos from a file
     """
     with open(path, 'r') as f:
-        lines = f.readlines()
-        return lines[1].strip()
+        data = f.read()
+    return tuple(re.findall(r'\n([ACGT]+)\n', data)[:n])
 
+n = 5
 input_files = glob('in/*')
 output_files = glob('raw_fastq/*')
 
-input_headers = [extract_header(f) for f in input_files]
-output_headers = [extract_header(f) for f in output_files]
+input_infos = [extract_n_first(f, n) for f in input_files]
+output_infos = [extract_n_first(f, n) for f in output_files]
 
+os.mkdir('simulated', exist_ok=True)
 
-similitude = 0
-for in_name, in_header in zip(input_files, input_headers):
-    for out_name, out_header in zip(output_files, output_headers):
-        if in_header == out_header:
-            similitude += 1
-            system(f'cp "{out_name}" "{in_name.replace("in", "aligned")}"')
-print(f"Found {similitude} similitudes")
+already_aligned = dict()
+for in_name, in_info in zip(input_files, input_infos):
+    for out_name, out_info in zip(output_files, output_infos):
+        if in_info == out_info:
+            if in_info in already_aligned:
+                print(f"{in_name} -> {out_name}")
+                print(f'Already aligned {in_info} with {already_aligned[in_info]}')
+                continue
 
-
+            already_aligned[in_info] = out_name
+            system(f'cp "{out_name}" "{in_name.replace("in", "simulated")}"')
